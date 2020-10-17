651f764696ac6d595befbdb707b5ad4e6dcdf50f
“Inayat61” <ainayat865@gmail.com>
2020-10-17 19:23:00 +0500

    done code

--- TASK.java
+++ TASK.java
@@ -1,5 +1,15 @@
 class Test{
  public static void main(String[] args){
- //code for even odd number
- }
+ //code for even odd number'
+for(int i=0;i<args.length;i++){
+	if(args[i]%2==0){
+		System.out.println("Even Number");
+	
+	}
+	else if(args[i]%2!=0){
+		System.out.println("Odd Number");
+	}
+
+}
+}
 }
